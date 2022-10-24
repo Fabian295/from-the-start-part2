48d65dfee2fc4d53203588ed168d0a1652ca7a6a
Fabian295 <fabiankanhai70@gmail.com>
2022-10-24 09:32:28 +0200

    Add Routes to App.js and <BrowserRouter> to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Routes, Route, Outlet } from 'react-router-dom';
+import BudgetSnob from "./BudgetSnob";
+import BudgetEdit from "./BudgetEdit";
+import BudgetAdd from "./BudgetAdd";
+import Header from "./Header";
 
 const App = () => {
 
   return (
     <div>
-      <h1>The App Component</h1>
+      <Header />
+      <h4 className="App-h4">The App Component</h4>
+      <Routes>
+        {/* <Route path="/" component={App} element={<App />} /> */}
+        <Route path="/" c
+        // omponent={BudgetSnob}
+         element={<BudgetSnob />} 
+         />
+        <Route path="/edit" 
+        // component={BudgetEdit} 
+        element={<BudgetEdit />} 
+        />
+        <Route  path="/add"
+        element={<BudgetAdd />}
+        />
+      </Routes>
+      {/* <Outlet /> */}
     </div>
   )
 }
